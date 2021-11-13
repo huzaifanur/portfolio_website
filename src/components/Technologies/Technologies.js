f5c57143562bbb0f5b8f7814fee9fee680312e77
huzaifa <huzaifa98nur@gmail.com>
2021-11-14 00:47:18 +0500

    tech

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend, } from 'react-icons/di';
+import { DiFirebase, DiReact,} from 'react-icons/di';
 import { AiOutlineCloudServer } from 'react-icons/ai';
 
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
