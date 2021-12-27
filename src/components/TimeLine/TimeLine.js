c36e550ff5bd1445888c46d4817eccae52476d3c
huzaifa <huzaifa98nur@gmail.com>
2021-12-27 13:24:34 +0500

    timeline update

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -1,23 +1,29 @@
-
-
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
 
 const Timeline = () => {
-
-
-
-
-
-
   return (
-    <Section  id="about">
-      
+    <Section id="about">
       <SectionTitle>About Me</SectionTitle>
       <SectionText>
-      I am a full stack engineer with a demonstrated history of working in the computer software industry. I love solving complex problems, writing clean code and I enjoy working in a wide set of stuff comprising both backend development and frontend development.
+        I am a full stack engineer with a demonstrated history of working in the
+        computer software industry. I love solving complex problems and writing
+        clean code.
       </SectionText>
-     
+      <SectionText>
+        I have around two years of experience as web developer. But experience
+        is not consistent as I did not have a job more than 3 months in this
+        period. I wan running my own startup and I also had a family situation
+        which needed me to travel between Lahore & Islamabad very frequently.
+      </SectionText>
+      <SectionText>
+        In the past year I have worked on many small projects remotley, worked
+        in a Software house onsite for 3 months. But Now I am looking for a full
+        time Job
+      </SectionText>
     </Section>
   );
 };
