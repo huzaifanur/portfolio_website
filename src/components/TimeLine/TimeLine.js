41dcba738b6c5ff61e3c8cd5e39cac01859295c2
huzaifa <huzaifa98nur@gmail.com>
2022-01-08 23:42:52 +0500

    cleanup

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -13,17 +13,6 @@ const Timeline = () => {
         computer software industry. I love solving complex problems and writing
         clean code.
       </SectionText>
-      <SectionText>
-        I have around two years of experience as web developer. But experience
-        is not consistent as I did not have a job more than 3 months in this
-        period. I wan running my own startup and I also had a family situation
-        which needed me to travel between Lahore & Islamabad very frequently.
-      </SectionText>
-      <SectionText>
-        In the past year I have worked on many small projects remotley, worked
-        in a Software house onsite for 3 months. But Now I am looking for a full
-        time Job
-      </SectionText>
     </Section>
   );
 };
